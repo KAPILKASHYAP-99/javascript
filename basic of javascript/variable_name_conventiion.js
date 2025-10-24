5710d227c60d1cf932bf7db1aa50c589d77629d4
KAPILKASHYAP-99 <kapilkashyap.inwork@gmail.com>
2025-10-24 20:13:18 +0530

    basic of javascript

---
+++ basic of javascript/variable_name_conventiion.js
@@ -0,0 +1,15 @@
+
+// let username;
+// let userAge;
+// let userHomeAddress;
+// let age;
+// let _;
+// let $ = "hello";
+// console.log($);
+// let number1;
+// let number_2;
+
+
+// 2 best practise
+// let userName = "kapil";
+// console.log(userName);
