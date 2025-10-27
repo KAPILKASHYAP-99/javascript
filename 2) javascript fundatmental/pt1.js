dddcd1119610fd7f5610e50fcd091eb10d40946f
KAPILKASHYAP-99 <kapilkashyap.inwork@gmail.com>
2025-10-27 12:47:49 +0530

    fundamental of javascript

---
+++ 2) javascript fundatmental/pt1.js
@@ -0,0 +1,28 @@
+// topic - comparison operator
+
+// console.log(50 > 30);
+// console.log(50 < 30);
+// console.log(30 >= 30);
+// console.log(40 == 40);
+// console.log(50 == 40);
+
+/**
+ * string
+ */
+// console.log("apple" > "banana");
+// console.log("glow" > "glowing");
+// console.log("gowing" > "glow");
+// console.log("2" > 1);
+// console.log("01" == 1 );
+
+/**
+ * Strict equality
+ */
+// console.log("01" === 1);
+// console.log(null == undefined);
+// console.log(null === undefined);
+// console.log(null > 0); // null become 0
+// console.log(null < 1);
+// console.log(null >= 0);
+// console.log(null == 0); // here null is null
+
---
+++ 2) javascript fundatmental/pt2.js
@@ -0,0 +1,220 @@
+//  topic - 2coditional statement
+// 1 if (true){
+//     console.log("hello, i am from gfg")
+// }
+//  2 if (false){
+//     console.log("hello, i am from gfg")
+// }
+
+//  3 const isLoggedIn = false;
+// if (isLoggedIn){
+//     console.log("hello, i am from gfg")
+// }
+//  4 const isLoggedIn = true;
+// if (isLoggedIn){
+//     console.log("hello, i am from gfg")
+// }
+
+// 5 const userAge = 18;
+// if (userAge > 16){
+//     console.log("hey you are an adult..");
+// }
+ 
+//6  const userAge = 8;
+// if (userAge > 16){
+//     console.log("hey you are an adult...")
+// } 
+
+//7 const userAge = 8;
+// if (userAge > 16){
+//     console.log("hey you are an adult");
+// }else{
+//     console.log("you are not a adult");
+// }
+
+// 8 const readlineSync = require("readline-sync");
+// const userAge = readlineSync.question("how old you are");
+// if (userAge > 16){
+//     console.log("hey you are an adult...");
+// }else{
+//     console.log("you are not an adult");
+// }
+
+// 9  const readlineSync = require("readline-sync");
+// const number = Number(readlineSync.question("enter a number? "));
+// const remainder = number % 3;
+// if (remainder === 0){
+//     console.log("fizz");
+// }
+
+// 10) const readlineSync = require("readline-sync");
+
+// const number = Number(readlineSync.question("enter a number? "));
+
+// const remainderAfterDivisionByThree = number % 3;
+// const remainderAfterDivisionByFive = number % 5;
+// if (remainderAfterDivisionByThree === 0){
+//     console.log("fizz");
+// }else if (remainderAfterDivisionByFive === 0){
+//     console.log("Buzz");
+// }
+
+// 11)  const readlineSync = require("readline-sync");
+
+// const number = Number(readlineSync.question("enter a number? "));
+
+// const remainderAfterDivisionByThree = number % 3;
+// const remainderAfterDivisionByFive = number % 5;
+// if (remainderAfterDivisionByThree === 0){
+//     console.log("fizz");
+// }if (remainderAfterDivisionByFive === 0){
+//     console.log("Buzz");
+// }
+//  note - if we entered 30 they will give output fizz and fuzz
+
+
+// 12 ) const readlineSync = require("readline-sync");
+
+// const number = Number(readlineSync.question("enter a number? "));
+
+// const remainderAfterDivisionByThree = number % 3;
+// const remainderAfterDivisionByFive = number % 5;
+// if (remainderAfterDivisionByThree === 0){
+//     console.log("fizz");
+// }else if (remainderAfterDivisionByFive === 0){
+//     console.log("Buzz");
+// }
+
+
+
+//  13 )const readlineSync = require("readline-sync");
+
+// const number = Number(readlineSync.question("enter a number? "));
+
+// const remainderAfterDivisionByThree = number % 3;
+// const remainderAfterDivisionByFive = number % 5;
+
+// if (remainderAfterDivisionByThree === 0 && remainderAfterDivisionByFive === 0){
+//     console.log("fizz")
+// }
+
+
+// 14)  const readlineSync = require("readline-sync");
+
+// const number = Number(readlineSync.question("enter a number? "));
+
+// const remainderAfterDivisionByThree = number % 3;
+// const remainderAfterDivisionByFive = number % 5;
+
+// if (remainderAfterDivisionByThree === 0 && remainderAfterDivisionByFive === 0){
+//     console.log("fizz");
+// }else if (remainderAfterDivisionByThree === 0 || remainderAfterDivisionByFive === 0){
+//     console.log("buzz");
+// }
+
+
+// 15  const readlineSync = require("readline-sync");
+
+// const number = Number(readlineSync.question("enter a number? "));
+
+// const remainderAfterDivisionByThree = number % 3;
+// const remainderAfterDivisionByFive = number % 5;
+
+// if (remainderAfterDivisionByThree === 0 && remainderAfterDivisionByFive === 0){
+//     console.log("fizz");
+// }if (remainderAfterDivisionByThree === 0 || remainderAfterDivisionByFive === 0){
+//     console.log("buzz");
+// }
+
+
+
+// 16  const readlineSync = require("readline-sync");
+// const number = Number(readlineSync.question("enter a number? "));
+// const remainderAfterDivisionByThree = number % 3;
+// const remainderAfterDivisionByFive = number % 5;
+// const remainderAfterDivisionBySeven = number % 7;
+// if (remainderAfterDivisionByThree === 0 && remainderAfterDivisionByFive === 0){
+//     console.log("fizz");
+// }else if (remainderAfterDivisionByThree === 0 || remainderAfterDivisionByFive === 0){
+//     console.log("buzz");
+// } else if (remainderAfterDivisionBySeven === 0){
+//     console.log("buzzbuzz");
+// }
+
+
+// 17 const readlineSync = require("readline-sync");
+// const number = Number(readlineSync.question("enter a number? "));
+// const remainderAfterDivisionByThree = number % 3;
+// const remainderAfterDivisionByFive = number % 5;
+// const remainderAfterDivisionBySeven = number % 7;
+// if (remainderAfterDivisionByThree === 0 && remainderAfterDivisionByFive === 0){
+//     console.log("fizz");
+// }else if (remainderAfterDivisionByThree === 0 || remainderAfterDivisionByFive === 0){
+//     console.log("buzz");
+// } else if (remainderAfterDivisionBySeven === 0){
+//     console.log("buzzbuzz");
+// }else{
+//     console.log("number is not divisible by 3,5, and 7");
+// }
+
+
+
+// 18  const readlineSync = require("readline-sync");
+// const number = Number(readlineSync.question("enter a number? "));
+// const remainderAfterDivisionByThree = number % 3;
+// const remainderAfterDivisionByFive = number % 5;
+// const remainderAfterDivisionBySeven = number % 7;
+// if (remainderAfterDivisionByThree === 0 && remainderAfterDivisionByFive === 0){
+//     console.log("fizz");
+// } if (remainderAfterDivisionByThree === 0 || remainderAfterDivisionByFive === 0){
+//     console.log("buzz");
+// } if (remainderAfterDivisionBySeven === 0){
+//     console.log("buzzbuzz");
+// }
+
+
+
+// 19  const readlineSync = require("readline-sync");
+// const number = Number(readlineSync.question("enter a number? "));
+// const remainderAfterDivisionByThree = number % 3;
+// const remainderAfterDivisionByFive = number % 5;
+// const remainderAfterDivisionBySeven = number % 7;
+// if (remainderAfterDivisionByThree === 0 && remainderAfterDivisionByFive === 0){
+//     console.log("fizz");
+// } if (remainderAfterDivisionByThree === 0 || remainderAfterDivisionByFive === 0){
+//     console.log("buzz");
+// } if (remainderAfterDivisionBySeven === 0){
+//     console.log("buzzbuzz");
+// }
+// else {
+//     console.log("not divisible by 3 , 5 and 7");
+// }
+
+
+
+// 20  const readlineSync = require("readline-sync");
+// const number = Number(readlineSync.question("enter a number? "));
+// const remainderAfterDivisionByThree = number % 3;
+// const remainderAfterDivisionByFive = number % 5;
+// const remainderAfterDivisionBySeven = number % 7;
+// if (remainderAfterDivisionByThree === 0 && remainderAfterDivisionByFive === 0){
+//     console.log("fizz");
+// } else if (remainderAfterDivisionByThree === 0 || remainderAfterDivisionByFive === 0){
+//     console.log("buzz");
+// } else if (remainderAfterDivisionBySeven === 0){
+//     console.log("buzzbuzz");
+// }
+// else {
+//     console.log("not divisible by 3 , 5 and 7");
+// }
+
+
+
+
+
+
+
+
+
+
+
