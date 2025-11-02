a97f40697d6fc925ff2a0b3ac958b40f4394af5a
KAPILKASHYAP-99 <kapilkashyap.inwork@gmail.com>
2025-11-02 13:53:21 +0530

    javascript

---
+++ 2) javascript fundatmental/pt6.js
@@ -0,0 +1,57 @@
+/**
+ * Logical Operator Part -1
+ */
+// 1 {And &&  } note -> all the conditions must be true then && will return true otherwise && will return false
+// const physics = 90;
+// const maths = 95;
+// const chemistry = 83;
+// const biology = 96;
+// if (physics > 85 && maths > 85 && chemistry > 85){
+//     console.log("you are eligible for eng....")
+// }else{
+//     console.log("you are not eligible for IIT..");
+// }
+
+// 2 {OR || operator } note --> if the atleast one condition  us true then || will return otherwise || will return false
+// const physics = 90;
+// const maths = 95;
+// const chemistry = 83;
+// const biology = 96;
+// if (physics > 90 || maths > 85 || chemistry > 89){
+//     console.log("you are eligible for engg....")
+// }else{
+//     console.log("you are not eligible for Enggg...")
+// }
+
+// 3 {NOT}
+// const isStudentEligible = false; // false
+// if (isStudentEligible){
+//     console.log("you are eligible ")
+// }else{
+//     console.log("you are not eligible")
+
+// }
+
+// const isStudentEligible = false; // false
+// if (!isStudentEligible){
+//     console.log("you are eligible ")
+// }else{
+//     console.log("you are not eligible")
+
+// }
+
+// const isStudentEligible = false; // false
+// if (!isStudentEligible){
+//     console.log(!isStudentEligible)
+//     console.log("you are eligible ")
+// }else{
+//     console.log("you are not eligible")
+
+// }
+
+
+
+
+
+
+
