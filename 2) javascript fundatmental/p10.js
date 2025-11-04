4b195845f8d1017477f84f1c6e270509689a1228
KAPILKASHYAP-99 <kapilkashyap.inwork@gmail.com>
2025-11-04 12:51:29 +0530

    javascript

---
+++ 2) javascript fundatmental/p10.js
@@ -0,0 +1,59 @@
+/**
+ * for loop
+ */
+// for (startValue; condition; incrementValue){
+
+// }
+// 1
+// for (let i=0; i<10; i++){
+//     console.log("Hello,......");
+// }
+// console.log("loop execution done!!!");
+
+// 2
+// for (let i=0; i<10; i++){
+//     console.log(i);
+//     console.log("Hello,...");
+// }
+// console.log("loop execution done !!!");
+
+// 3
+// for (let i=1; i<100; i++){
+//     console.log(i);
+
+// }
+// console.log("loop execution done !!!");
+
+// 4
+// const userName = "kapil kashyap";
+// console.log(userName[0]);
+// console.log(userName[1]);
+
+// 5
+// const userName = "kapil kashyap";
+// for (let i=0; i<10; i++){
+//     console.log(i);
+// }
+// const stringLength = userName.length;
+// console.log(stringLength);
+
+// 6
+// const userName = "kapil kashyap";
+// const stringLength = userName.length;
+// for (let i=0; i<stringLength; i++){
+//     console.log(userName[i]);
+// }
+
+// 7
+// const userName = "kapil";
+// const stringLength = userName.length;
+// for (let i=0; i<stringLength; i++){
+//     console.log(userName[i]);
+// }
+
+// 8
+const userName = "kapil kashyap";
+const stringLength = userName.length;
+for (let i=0; i<stringLength; i++){
+    console.log(userName[i]);
+}
---
+++ 2) javascript fundatmental/pt9.js
@@ -0,0 +1,28 @@
+/**
+ * Nullish Coalescing
+ */
+// 1
+// const firstName = "kapil";
+// console.log(firstName);
+// 2
+// const firstName = "kapil";
+// console.log(firstName ?? "HiddenGeek");
+// 3
+// let firstName;
+// let firstName = "";
+let firstName = 0;
+// console.log(firstName ?? "HiddenGeek");
+// console.log(null ?? "HiddenGeek");
+// console.log(firstName || "HiddenGeek");
+console.log(firstName ?? "HiddenGeek");
+
+// 4
+// const a = 0;
+// console.log(a || 1);
+// console.log(a ?? 1);
+
+// 5
+// let a = 12;
+// let b;
+// console.log(a + (b || 0));
+// console.log(a + (b ?? 0));
