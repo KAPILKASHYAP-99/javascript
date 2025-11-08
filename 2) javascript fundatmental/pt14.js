c1e54341e761174165fce5d09e557bec2a891a68
KAPILKASHYAP-99 <kapilkashyap.inwork@gmail.com>
2025-11-08 12:07:52 +0530

    javascript

---
+++ 2) javascript fundatmental/pt14.js
@@ -0,0 +1,18 @@
+/**
+ * while loop
+ * do while loop
+ */
+// 1
+// let i = 0;
+// while (i < 10){
+//     console.log(i);
+//     i = i + 1
+// }
+// console.log("execution done!!!")
+
+// 2 do while loop
+// let i = 0;
+// do {
+//     console.log(i);
+//     i++;
+// }while(i < 10)
