8ebc5776a0c20af13fa295f9962978bd85cda879
KAPILKASHYAP-99 <kapilkashyap.inwork@gmail.com>
2025-11-23 13:37:48 +0530

    JAVASCRIPT

---
+++ 3) string/pt1.js
@@ -0,0 +1,64 @@
+// topic 1 -> Iterating over string
+// 1 
+// let displayMessage = " i am mentor at geekforgeeks"
+
+// for (let char of displayMessage){
+//     //1  console.log(char);
+    
+//     if(char === "n"){
+//         break;
+//     }else{
+//         console.log(char)
+//     }
+// }
+
+// 2
+// let displayMessage = " i am mentor at geeksforgeeks"
+// let count = 0;
+// for (let char of displayMessage){
+//     if (char === "a"){
+//         count++;
+//     }    
+
+// }
+// console.log(count);
+
+
+// 3
+// let displayMessage = "I  am mentor at geeksforgeeks"
+// console.log(displayMessage[0]);
+// console.log(displayMessage[1]);
+// console.log(displayMessage[2]);
+// console.log(displayMessage[3]);
+
+// 4 
+// let displayMessage = "I  am mentor at geeksforgeeks"
+
+// for (let i=0; i<displayMessage.length; i++){
+//     console.log(displayMessage[i])
+// }
+
+// 5
+// let displayMessage = "I  am mentor at geeksforgeeks"
+
+// for (let i=0; i<displayMessage.length; i++){
+//     if (displayMessage[i] === "n"){
+//         break;
+//     }else{
+//         console.log(displayMessage[i])
+//     }
+
+// }
+
+// 6
+let displayMessage = " i am mentor at geeksforgeeks"
+for (let i=0; i<displayMessage.length; i++){
+    if (displayMessage[i] === "r"){
+        console.log(i);
+    }
+}
+
+
+
+
+
