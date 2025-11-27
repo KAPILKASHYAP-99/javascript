8d55878cdf8821aa1222209e379d9e19f55eea55
KAPILKASHYAP-99 <kapilkashyap.inwork@gmail.com>
2025-11-27 12:03:27 +0530

    javascript

---
+++ 3) functional programing/pt2.js
@@ -0,0 +1,35 @@
+// topic 2 Anonymous function
+// 1
+// let anonymousFunction = function(){
+//     console.log("Hello from gfg");
+// }
+// anonymousFunction();
+// 2
+// let anonymousFunction = function(){
+//     console.log("Hello from gfg");
+// }
+// console.log(typeof anonymousFunction);
+// anonymousFunction();
+
+// 3
+// var anonymousFunction = function(){
+//     console.log("Hello from gfg");
+// }
+// console.log(typeof anonymousFunction);
+// anonymousFunction();
+
+// 4
+// let anonymousFunction = function greetMessage(){
+//     console.log("hello from gfg");
+// }
+// console.log(typeof anonymousFunction);
+// anonymousFunction();
+
+// 5
+let anonymousFunction = function greetMessage(){
+    console.log("hello from gfg"); // hello from gfg
+    console.log(typeof greetMessage) // function
+
+}
+console.log(typeof anonymousFunction); // function
+anonymousFunction();
