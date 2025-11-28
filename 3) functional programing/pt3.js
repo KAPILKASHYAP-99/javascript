846617867c92bcd708f4d0cccba8f7ff6e08d08a
KAPILKASHYAP-99 <kapilkashyap.inwork@gmail.com>
2025-11-28 12:02:13 +0530

    javascript

---
+++ 3) functional programing/pt3.js
@@ -0,0 +1,28 @@
+// chapter - Return and undefined
+// 1
+// function calculateSum(x,y){
+//     console.log(x + y);
+// }
+// calculateSum(10,20);
+
+// 2
+// function calculateSum(x,y){
+//     return x + y
+// }
+// console.log(calculateSum(12,15));
+
+// 3
+// function calculateSum(x,y){
+//     return x + y
+// }
+// const result = calculateSum(12,15);
+// console.log(result);
+
+// 4
+function calculateSum(x,y){
+    return x + y
+}
+const result = calculateSum(12,15);
+console.log(result);
+const output = calculateSum(99,100);
+console.log(output);
