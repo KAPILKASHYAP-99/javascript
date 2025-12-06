3ab5ae8d3c30cf8f379d3bc36a37de70e1c51ca7
KAPILKASHYAP-99 <kapilkashyap.inwork@gmail.com>
2025-12-06 12:08:07 +0530

    javascript

---
+++ 3) functional programing/pt4.js
@@ -0,0 +1,14 @@
+// topic - Arrow function
+// 1 
+// const calculateAddition = (x,y) => x + y
+// console.log(calculateAddition(12,13));
+
+
+
+// 2
+// const Sum = (x,y) => x > y ? x + y : x - y 
+// const output = Sum(15,10)
+// console.log(output)
+
+
+// 3
